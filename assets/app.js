172d07bb368a0227a81affaf06d43b764da34387
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-03-04 11:42:33 -0400

    Update employee role working

--- assets/app.js
+++ assets/app.js
@@ -17,13 +17,14 @@ const init = () => {
           "Add a department",
           "Add a role",
           "Add an employee",
-        //   "Update an employee role",
+          "Update an employee role",
         //   "Update an employee's manager",
         //   "View employees by manager",
         //   "View employees by department",
         //   "Remove a department",
         //   "Remove a role",
         //   "Remove an employee",
+        //   "View the total utilized budget"
           "Exit"
         ]
       }
@@ -42,6 +43,16 @@ const init = () => {
         }
         else if( response == "Add a department"){
             addDepartment();
+            
+        }
+        else if( response == "Add a role"){
+            addRole();
+        }
+        else if( response == "Add an employee"){
+            addEmployee();
+        }
+        else if( response == "Update an employee role"){
+            updateEmployeeRole();
         }
     })
 }
@@ -105,9 +116,111 @@ function addDepartment () {
             }
         });
     })
-   
+    };
 
-}
+//     INSERT INTO roles (title, salary, department_id)
+// VALUES 
+// ("Head of Sales", 150000, 1), 
+
+    function addRole () {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "newTitle",
+                message: "Please enter the new job title: ",
+            },
+    
+            {
+                type: "input",
+                name: "newSalary",
+                message: "Please enter the salary: ",
+            },
+    
+            {
+                type: "input",
+                name: "departmentId",
+                message: "Please enter the department id: ",
+            }
+        ])
+        .then(data => {
+            const sql = `INSERT INTO role SET ?`;
+            const values = {title: data.newTitle, salary: data.newSalary, department_id: data.departmentId};
+            db.query(sql, values, (err) => {
+                if (err){
+                    throw err
+                }
+                else {
+                    console.log("New role added");
+                    return viewRoles();
+                }
+            });
+        }) 
+    };
+
+ function addEmployee () {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "fname",
+            message: "Please enter the first name of the employee: ",
+        },
+
+        {
+            type: "input",
+            name: "lname",
+            message: "Please enter the last name of the employee: ",
+        },
+
+        {
+            type: "input",
+            name: "roleId",
+            message: "Please enter the role id of the employee: ",
+        }
+    ])
+    .then(data => {
+        const sql = `INSERT INTO employee SET ?`;
+        const values = {first_name: data.fname, last_name: data.lname, role_id: data.roleId};
+        db.query(sql, values, (err) => {
+            if (err){
+                throw err
+            }
+            else {
+                console.log("New employee added");
+                return viewEmployees();
+            }
+        });
+    }) 
+};
+
+function updateEmployeeRole () {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "employeeId",
+            message: "Please enter the id of the employee: ",
+        },
+
+        {
+            type: "input",
+            name: "newRole",
+            message: "Please enter the role id of the employee: ",
+        }
+    ])
+    .then(data => {
+        const sql = `UPDATE employee SET role_id = ${data.newRole} WHERE id = ${data.employeeId}`;
+        //const values = {role_id: data.newRole, id: data.employeeId};
+        db.query(sql, (err) => {
+            if (err){
+                throw err
+            }
+            else {
+                console.log("Employee role updated");
+                return viewEmployees();
+            }
+        });
+    }) 
+};
+
 
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,7 +5,7 @@ VALUES
  ("Production"),
  ("Finance");
 
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES 
 ("Head of Sales", 150000, 1), 
 ("Salesperson", 85000, 1), 
@@ -13,7 +13,7 @@ VALUES
 ("Production Analyst", 100000, 2), 
 ("Accountant", 75000, 3);
 
-INSERT INTO employees (first_name, last_name, role_id)
+INSERT INTO employee (first_name, last_name, role_id)
 VALUES 
 ("The", "People", 1), 
 ("No", "Jokes", 2), 
